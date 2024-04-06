4ebe02878cbc56b45a63cf0d623e4c64c205e0b0
Alexandra Timohina <128403527+Alexaxaxaxaxa@users.noreply.github.com>
2024-04-06 22:02:55 +0300

    sem1

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header =() => {
+    return (
+        <div>
+            <header> Hello </header>
+            <p> ehfvdefd ewfdserf </p>
+        </div>
+    );
+}
+
+export default Header;
